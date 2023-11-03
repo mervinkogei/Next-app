601af12ec21b8e964734db6459d24909528a3cf9
mervinkogei <vincent.kogei@yahoo.com>
2023-11-03 11:18:49 +0300

    added a footer and navbar component

---
+++ app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogLayout = ({children}) => {
+  return (
+    <div>
+        <h1>This is a blog</h1>
+        {children}</div>
+  )
+}
+
+export default BlogLayout
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 import Link from 'next/link'
-import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
-   <main><h1>Hello Wold</h1>
-   <Link href='/users'>Users</Link>
-   <ProductCard/>
+   <main>
+    <h1>Hello Wolrd</h1>
+   {/* <Link href='/users'>Users</Link>  */}
+  
    </main>
   )
 }
