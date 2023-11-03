2ed2f390be6aa04457895c37b3555f3f389c13f7
mervinkogei <vincent.kogei@yahoo.com>
2023-11-03 12:43:20 +0300

    navbar styling

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
 
 
 const links = [
@@ -33,13 +34,13 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href ="/">Lamia</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href ="/" className={styles.logo}>Lamia</Link>
+        <div className={styles.links}>
             {links.map(link =>(
-                <Link key={link.id} href={link.url}>{link.title}</Link>
+                <Link key={link.id} href={link.url} className={styles.link}>{link.title}</Link>
             ))}
-            <button onClick={()=> console.log('Logout button')}>Logout</button>
+            <button onClick={()=> console.log('Logout button')} className={styles.logout}>Logout</button>
         </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
         </body>
     </html>
   )
