62a14594629fb47b26feeb1e995906339165a75f
mervinkogei <vincent.kogei@yahoo.com>
2023-11-03 15:16:19 +0300

    update about page and custom button component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
+    <div className={styles.container}>
+     <div className={styles.imgContainer}>
+      <Image className={styles.img} src='https://images.pexels.com/photos/2832034/pexels-photo-2832034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' fill={true} alt=''/>
+      <div className={styles.imgText}>
+        <h1 className={styles.imgTitle}>Digital StoryTellers</h1>
+        <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+      </div>
+     </div>
+     <div className={styles.textContainer}>
+       <div className={styles.item}>
+          <h1 className={styles.title}>Who are we?</h1>
+          <p className={styles.desc}>The electronic computer is one of the most important developments of the twentieth century. Like the
+            industrial revolution of the nineteenth century, the computer and the information and communication
+            technology built upon it have drastically changed business, culture, government and science, and have
+            touched nearly every aspect of our lives</p>
+       </div>
+       <div className={styles.item}>
+       <h1 className={styles.title}>Who are we?</h1>
+          <p className={styles.desc}>The electronic computer is one of the most important developments of the twentieth century. Like the
+            industrial revolution of the nineteenth century, the computer and the information and communication
+            technology built upon it have drastically changed business, culture, government and science, and have
+            touched nearly every aspect of our lives</p>
+       </div>
+
+     </div>
+     
+     </div>
   )
 }
 
